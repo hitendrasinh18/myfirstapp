379c62d6eb90ab9ad4de9cb46afc99943cbbcc81
hitendrasinh18 <hitendrasinh2003@gmail.com>
2024-08-12 13:38:40 +0530

    commit on 12/08/2024

--- src/screen/Fooddelivery.js
+++ src/screen/Fooddelivery.js
@@ -8,6 +8,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const Fooddelivery = () => {
     return (
 <SafeAreaView>
+<ScrollView >
+
         <View style=
             {{
                 flexDirection: "row",
@@ -27,35 +29,98 @@ const Fooddelivery = () => {
 
         {/* food item type */}
         <Text style={{margin:30,justifyContent:"flex-start",alignItems:"flex-start"}}>HITENDRASINH, WHAT'S ON YOUR MIND</Text>
-        <ScrollView showsHorizontalScrollIndicator={false}>
+        <View style={styles.fooditem}>
 
-<View style={styles.fooditem}>
-    <View style={styles.foodimage}>
+    {/* <View style={styles.foodimage}> */}
     <Image style={{flexDirection:"row", justifyContent:"space-between",
 width:60,height:60,borderRadius:30,}} source={require("../assets/biriyani.jpg")}/>
-</View>
-    {/* <Text>Biriyani</Text> */}
+
+{/* </View> */}
     
-    <View style={styles.foodimage}>
+    {/* <View style={styles.foodimage}> */}
     <Image style={{flexDirection:"row", justifyContent:"space-between",
 width:60,height:60,borderRadius:30,}} source={require("../assets/pizzas.jpg")}/>
-</View>
-    {/* <Text>Pizzas</Text> */}
-    <View style={styles.foodimage}>
+
+{/* </View> */}
+    {/* <View style={styles.foodimage}> */}
     <Image style={{flexDirection:"row", justifyContent:"space-between",
 width:60,height:60,borderRadius:30,}} source={require("../assets/paratha.jpg")}/>
-</View>
-    {/* <Text>Paratha</Text> */}
+
+{/* </View> */}
     
-    <View style={styles.foodimage}>
+    {/* <View style={styles.foodimage}> */}
     <Image style={{flexDirection:"row", justifyContent:"space-between",
 width:60,height:60,borderRadius:30,}} source={require("../assets/vadapav.jpg")}/>
-</View>
-    {/* <Text>Vadapav</Text> */}
+
+{/* </View> */}
+
     </View>
-    </ScrollView>
 
 
+    <View style={styles.foodtext}>
+    <Text>Biriyani</Text>
+    <Text>Pizzas</Text>
+
+    <Text >Paratha</Text>
+    <Text>Vadapav</Text>
+    </View>
+    <View>
+        <Text style={{marginTop:15, marginLeft:10, color:"orange",fontWeight:"bold" }}>WELCOME OFFER!!</Text>
+        <Text style={{fontWeight:"bold", marginLeft:10, fontSize:25, fontStyle:"italic"}}>Get 50% off &</Text>
+        <Text style={{ fontWeight:"bold", marginLeft:10, fontSize:25, fontStyle:"italic"}}>free delivery</Text>
+         <Text style={{marginLeft:10}}>on your first order on swiggy!</Text>
+     </View>
+     
+<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+     <View style={styles.foodstore}>
+    <Image style={{height:200, width:150, borderRadius:10, margin:10}} source={require("../assets/food.jpg")}/>
+    <Text style={{position:"absolute", bottom:25,left:10,fontSize:20,color:"white", fontWeight:700}}> 50% OFF</Text>
+    <Text style={{position:"absolute", bottom:10 , left:10,color:"white", fontSize:10 }}> UP TO RS.100</Text>
+</View>
+<View style={styles.foodstore}>
+
+    <Image style={{height:200, width:150, borderRadius:10, margin:10,}} source={require("../assets/icecream.jpg")}/>
+    <Text style={{position:"absolute", bottom:25,left:10,fontSize:20,color:"white", fontWeight:700}}> 60% OFF</Text>
+    <Text style={{position:"absolute", bottom:10,left:10,fontSize:10,color:"white", }}> UP TO RS.100</Text>
+
+</View>
+<View style={styles.foodstore}>
+
+    <Image style={{height:200, width:150, borderRadius:10, margin:10,}} source={require("../assets/mithai.jpg")}/>
+     <Text style={{position:"absolute", bottom:25,left:10,fontSize:20,color:"white", fontWeight:700}}> 50% OFF</Text>
+     <Text style={{position:"absolute", bottom:10,left:10,fontSize:10,color:"white",}}> UP TO RS.100</Text>
+
+     </View>
+     </ScrollView>
+     <Text style={{left:10}}>POPULAR BRANDS</Text>
+     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+     <View style={styles.foodstore}>
+
+    <Image style={{height:200, width:150, borderRadius:10, margin:10}} source={require("../assets/food.jpg")}/>
+    <Text style={{position:"absolute", bottom:25,left:10,fontSize:20,color:"white", fontWeight:700}}> 50% OFF</Text>
+    <Text style={{position:"absolute", bottom:10 , left:10,color:"white", fontSize:10 }}> UP TO RS.100</Text>
+
+</View>
+<View style={styles.foodstore}>
+
+    <Image style={{height:200, width:150, borderRadius:10, margin:10,}} source={require("../assets/icecream.jpg")}/>
+    <Text style={{position:"absolute", bottom:25,left:10,fontSize:20,color:"white", fontWeight:700}}> 60% OFF</Text>
+    <Text style={{position:"absolute", bottom:10,left:10,fontSize:10,color:"white", }}> UP TO RS.100</Text>
+
+</View>
+<View style={styles.foodstore}>
+
+    <Image style={{height:200, width:150, borderRadius:10, margin:10,}} source={require("../assets/mithai.jpg")}/>
+     <Text style={{position:"absolute", bottom:25,left:10,fontSize:20,color:"white", fontWeight:700}}> 50% OFF</Text>
+     <Text style={{position:"absolute", bottom:10,left:10,fontSize:10,color:"white",}}> UP TO RS.100</Text>
+
+
+     </View>
+     </ScrollView>
+    
+
+
+</ScrollView>
  </SafeAreaView>
  
     )
@@ -64,12 +129,24 @@ width:60,height:60,borderRadius:30,}} source={require("../assets/vadapav.jpg")}/
 export default Fooddelivery;
 
 const styles = StyleSheet.create({
-    fooditem:{
-        flex:1,
-        flexDirection:"row",
-        justifyContent:"space-between",
-        marginTop:20,
- },
+
+fooditem:{
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"space-around",
+    
+},
+foodtext:{
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"space-around"
+},
+foodstore:{
+    flexDirection:"row",
+    margin:10,
+},
+
    
-})
+}
+)
 
